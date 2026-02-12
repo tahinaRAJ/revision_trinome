133b32b53bf6e80d864fc0b38188fadaa1ebe671
ayman-hamidullah <hamidullahaymanzara@gmail.com>
2026-02-12 13:33:52 +0300

    routage pages

--- app/controllers/RedirectController.php
+++ app/controllers/RedirectController.php
@@ -7,4 +7,34 @@ class RedirectController {
     ]);
   }
 
+  public static function redirectHome ($page) {
+    if (file_exists('app/views/home/' . $page . '.php')) {
+      Flight::render('home/' . $page);
+    } else {
+      Flight::render('home/index', [
+        'success' => false
+      ]);
+    }
+  }
+
+  public static function redirectPages ($page) {
+    if (file_exists('app/views/' . $page . '.php')) {
+      Flight::render($page);
+    } else {
+      Flight::render('home/index', [
+        'success' => false
+      ]);
+    }
+  }
+
+  public static function redirectShop ($page) {
+    if (file_exists('app/views/shop/' . $page . '.php')) {
+      Flight::render('shop/' . $page);
+    } else {
+      Flight::render('home/index', [
+        'success' => false
+      ]);
+    }
+  }
+
 }
--- app/routes.php
+++ app/routes.php
@@ -6,6 +6,16 @@ require_once __DIR__ . '/repositories/UserRepository.php';
 require_once __DIR__ . '/controllers/RedirectController.php';
 
 Flight::route('GET /', ['RedirectController', 'redirectAccueil']);
+
+// Routes pour les pages d'accueil
+Flight::route('GET /home/@page', ['RedirectController', 'redirectHome']);
+
+// Routes pour les pages génériques
+Flight::route('GET /pages/@page', ['RedirectController', 'redirectPages']);
+
+// Routes pour les pages de la boutique
+Flight::route('GET /shop/@page', ['RedirectController', 'redirectShop']);
+
 Flight::route('GET /register', ['AuthController', 'showRegister']);
 Flight::route('POST /register', ['AuthController', 'postRegister']);
 Flight::route('POST /api/validate/register', ['AuthController', 'validateRegisterAjax']);
